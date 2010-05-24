7758e1d51bb733aa378fd774652546c3df172ccb
rookieone <rookieone@gmail.com>
2010-05-24 12:32:30 -0500

    using Funcs with SM

---
+++ Learning_StructureMap/Basics/SpecifyConstructionWithDependencies.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using StructureMap;
+using TestUtilities.Extensions;
+
+namespace Basics
+{
+    [TestFixture]
+    public class SpecifyConstructionWithDependencies
+    {
+        [SetUp]
+        public void setup()
+        {
+            _container = new Container();
+
+            _container.Configure(x =>
+                                     {
+                                         x.For<IFoo>().Use<Foo>();
+                                         x.For<IBar>().Use(c =>
+                                                               {
+                                                                   var bar = new Bar();
+                                                                   bar.Foo = c.GetInstance<IFoo>();
+                                                                   return bar;
+                                                               });
+                                     });
+        }
+
+        IContainer _container;
+
+
+        class Bar : IBar
+        {
+            public IFoo Foo { get; set; }
+        }
+
+        class Foo : IFoo
+        {
+        }
+
+        interface IBar
+        {
+            IFoo Foo { get; }
+        }
+
+        interface IFoo
+        {
+        }
+
+        [Test]
+        public void should_set_foo_property_on_bar()
+        {
+            var bar = _container.GetInstance<IBar>();
+
+            bar.Foo.ShouldBeType<Foo>();
+        }
+    }
+}
---
+++ Learning_StructureMap/Basics/SpecifyingConstruction.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using StructureMap;
+using TestUtilities.Extensions;
+
+namespace Basics
+{
+    [TestFixture]
+    public class SpecifyingConstruction
+    {
+        class Foo : IFoo
+        {
+            public Foo(string message)
+            {
+                Message = message;
+            }
+
+            public string Message { get; set; }
+        }
+
+        interface IFoo
+        {
+            string Message { get; }
+        }
+
+        [SetUp]
+        public void setup()
+        {
+            _container = new Container();
+            _container.Configure(x => x.For<IFoo>().Use(() => new Foo("Hello")));
+        }
+
+        IContainer _container;
+
+        [Test]
+        public void IFoo_should_resolve_to_Foo()
+        {
+            var foo = _container.GetInstance<IFoo>();
+
+            foo.ShouldBeType<Foo>();            
+        }
+
+        [Test]
+        public void message_should_be_set_during_construction()
+        {
+            var foo = _container.GetInstance<IFoo>();
+            
+            foo.Message.ShouldBe("Hello");
+        }
+    }
+}
